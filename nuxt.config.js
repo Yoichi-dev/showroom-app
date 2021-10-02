250011de7dc6dc6bb8296fe80aaffd7540596ac2
Yoichi-dev <yoichi.developer@gmail.com>
2021-10-02 11:21:03 +0900

    [Update] OGP修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,11 +11,14 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Point History | SRイベントポイント遷移確認ツール' },
+      { hid: 'og:title', property: 'og:title', content: 'Point History | SRイベントポイント遷移確認ツール' },
+      { hid: 'og:url', property: 'og:url', content: 'https://point-history.showroom-app.com' },
       { hid: 'description', name: 'description', content: 'SHOWROOMイベントのポイント遷移が確認できる個人制作Webアプリになります' },
-      { hid: 'og:title', property: 'og:title', content: 'Point History' },
-      { hid: 'twitter:card', property: 'twitter:card', content: 'summary_large_image' },
-      { hid: 'og:image', property: 'og:image', content: 'https://point-history.showroom-app.com/og.png' },
-      { hid: 'twitter:creator', property: 'twitter:creator', content: '@yoichiro_sub' },
+      { hid: 'og:image', property: 'og:image', content: 'https://point-history.showroom-app.com/og.png?20211001' },
+      { name: 'twitter:card', property: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:creator', property: 'twitter:creator', content: '@TYoichiro' },
+      { name: 'twitter:site', property: 'twitter:site', content: '@TYoichiro' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
