3c81dc336297c488b5a65e25de53b431f384f133
Yoichi-dev <yoichi.developer@gmail.com>
2023-07-06 21:27:30 +0900

    [Delete] url

--- components/Info.vue
+++ components/Info.vue
@@ -2,8 +2,6 @@
   <div>
     <h1>This HP is SHOWROOM App (Personal Works)</h1>
     <p><a href="https://watch-log.showroom-app.com/" target="_blank">Watch Log</a></p>
-    <p><a href="https://point-history.showroom-app.com/" target="_blank">Point History</a></p>
-    <p><a href="https://showtube.showroom-app.com/" target="_blank">Show Tube</a></p>
     <p><a href="https://github.com/Yoichi-dev/showroom-app" target="_blank">GitHub</a></p>
   </div>
 </template>
